7d9eca94fa1cc668fdb61a7b26fd000bf50da7d1
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-08 07:01:36 +0530

    Update memify.py

--- MahakRobot/modules/memify.py
+++ MahakRobot/modules/memify.py
@@ -3,8 +3,8 @@ import textwrap
 
 from PIL import Image, ImageDraw, ImageFont
 
-from Mahakxbot import telethn as bot
-from Mahakxbot.events import register
+from MahakRobot import telethn as bot
+from MahakRobot.events import register
 
 
 @register(pattern="^/mmf ?(.*)")
@@ -62,7 +62,7 @@ async def drawText(image_path, text):
 
     else:
 
-        fnt = "./Mahakxbot/resources/default.ttf"
+        fnt = "./MahakRobot/resources/default.ttf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
