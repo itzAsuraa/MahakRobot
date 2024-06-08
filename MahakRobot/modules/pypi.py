a819c9ad0a42b07fffefda69f8ab68d1fd12f4bd
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-08 13:21:03 +0530

    Update pypi.py

--- MahakRobot/modules/pypi.py
+++ MahakRobot/modules/pypi.py
@@ -5,7 +5,7 @@ from MahakRobot import pbot as app
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/avishaxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/mahakxbot?startgroup=true"),
     ],
 ]
 
