d4b62711252ad41d37c07a36d5e3e28c5a8f6b2e
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-08 09:38:39 +0530

    Update muting.py

--- MahakRobot/modules/muting.py
+++ MahakRobot/modules/muting.py
@@ -6,20 +6,20 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.utils.helpers import mention_html
 
-from Mahakxbot import LOGGER, TIGERS, dispatcher
-from Mahakxbot.modules.helper_funcs.chat_status import (
+from MahakRobot import LOGGER, TIGERS, dispatcher
+from MahakRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from Mahakxbot.modules.helper_funcs.extraction import (
+from MahakRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from Mahakxbot.modules.helper_funcs.string_handling import extract_time
-from Mahakxbot.modules.log_channel import loggable
+from MahakRobot.modules.helper_funcs.string_handling import extract_time
+from MahakRobot.modules.log_channel import loggable
 
 
 def check_user(user_id: int, bot: Bot, chat: Chat) -> Optional[str]:
