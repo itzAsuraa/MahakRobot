eb299d6a99c0b486a229c41f38de350a8e2a66b6
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-08 07:02:25 +0530

    Update must_join.py

--- MahakRobot/modules/must_join.py
+++ MahakRobot/modules/must_join.py
@@ -2,7 +2,7 @@ import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from Mahakxbot import pbot as app
+from MahakRobot import pbot as app
 
 #--------------------------
 MUST_JOIN = "Ravan_Lankaa"
