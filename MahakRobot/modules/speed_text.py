19ae0783856b43eca0cb762964b3b4858dcdfc75
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-08 20:37:27 +0530

    Update speed_text.py

--- MahakRobot/modules/speed_text.py
+++ MahakRobot/modules/speed_text.py
@@ -45,10 +45,10 @@ def speedtestxyz_callback(update: Update, context: CallbackContext):
 
         elif query.data == "speedtest_text":
             result = speed.results.dict()
-            replymsg += f"\n\n⬤ ᴅᴏᴡɴʟᴏᴀᴅ ➥ `{convert(result['download'])} ᴍʙ/s`\n● ᴜᴘʟᴏᴀᴅ ➥ `{convert(result['upload'])}ᴍʙ/s`\n● ᴘɪɴɢ ➥ `{result['ping']}`\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴀᴠɪsʜᴀ ꭙ ʀᴏʙᴏᴛ™ ♡゙"
+            replymsg += f"\n\n⬤ ᴅᴏᴡɴʟᴏᴀᴅ ➥ `{convert(result['download'])} ᴍʙ/s`\n● ᴜᴘʟᴏᴀᴅ ➥ `{convert(result['upload'])}ᴍʙ/s`\n● ᴘɪɴɢ ➥ `{result['ping']}`\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴍᴀʜᴀᴋ ꭙ ʀᴏʙᴏᴛ™ ♡゙"
             update.effective_message.edit_text(replymsg, parse_mode=ParseMode.MARKDOWN)
     else:
-        query.answer("⬤ ʏᴏᴜ ᴀʀᴇ ʀᴇǫᴜɪʀᴇᴅ ᴛᴏ ᴊᴏɪɴ @the_friendz ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
+        query.answer("⬤ ʏᴏᴜ ᴀʀᴇ ʀᴇǫᴜɪʀᴇᴅ ᴛᴏ ᴊᴏɪɴ @Ravan_Lankaa ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
 
 
 SPEED_TEST_HANDLER = DisableAbleCommandHandler(
