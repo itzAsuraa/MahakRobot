0f4314c5ee137fd974fd61dda2f73b04974dd223
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-08 10:29:08 +0530

    Update pokemon.py

--- MahakRobot/modules/pokemon.py
+++ MahakRobot/modules/pokemon.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from Mahakxbot import pbot as app
+from MahakRobot import pbot as app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.enums import ChatAction, ParseMode
 
