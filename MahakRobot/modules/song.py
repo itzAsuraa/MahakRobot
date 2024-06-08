c607b1d910a0fd5f2d76588fc0a7c9355c660d03
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-08 20:56:40 +0530

    Update song.py

--- MahakRobot/modules/song.py
+++ MahakRobot/modules/song.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**❖ sᴏɴɢ ᴛɪᴛʟᴇ ➥** {title[:25]}\n\n**● ᴅᴜʀᴀᴛɪᴏɴ ➥** {duration}\n**● ᴠɪᴇᴡs ➥** {views}\n\n**❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴀᴠɪsʜᴀ ꭙ ʀᴏʙᴏᴛ™ ♡゙**"
+        rep = f"**❖ sᴏɴɢ ᴛɪᴛʟᴇ ➥** {title[:25]}\n\n**● ᴅᴜʀᴀᴛɪᴏɴ ➥** {duration}\n**● ᴠɪᴇᴡs ➥** {views}\n\n**❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴍᴀʜᴀᴋ ꭙ ʀᴏʙᴏᴛ™ ♡゙**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
