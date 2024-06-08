fb4a9edcac1cfa104d25b06ff4f08dfdf058fcbe
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-08 06:47:54 +0530

    Update log_channel.py

--- MahakRobot/modules/log_channel.py
+++ MahakRobot/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from Mahakxbot.modules.helper_funcs.misc import is_module_loaded
+from MahakRobot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue
     from telegram.utils.helpers import escape_markdown
 
-    from Mahakxbot import EVENT_LOGS, LOGGER, dispatcher
-    from Mahakxbot.modules.helper_funcs.chat_status import user_admin
-    from Mahakxbot.modules.sql import log_channel_sql as sql
+    from MahakRobot import EVENT_LOGS, LOGGER, dispatcher
+    from MahakRobot.modules.helper_funcs.chat_status import user_admin
+    from MahakRobot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
