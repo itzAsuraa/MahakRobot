a14d7d70c12966048968f5053a3a97a9007f5723
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-09 15:07:21 +0530

    Update __main__.py

--- MahakRobot/__main__.py
+++ MahakRobot/__main__.py
@@ -832,7 +832,7 @@ def Music_about_callback(update: Update, context: CallbackContext):
             ),
         )
     elif query.data == "Music_roy_admin":
-        query.message.edit_caption(f"*✿ ᴄᴏᴍᴍᴀɴᴅꜱ ✿*",
+        query.message.edit_caption(f"*✿ ᴄᴏᴍᴍᴀɴᴅꜱ ✿*"
             f"""\n\n ᴄᴏᴍᴍɪᴍɢ sᴏᴏɴ""",
             parse_mode=ParseMode.MARKDOWN,
             
@@ -845,7 +845,7 @@ def Music_about_callback(update: Update, context: CallbackContext):
             ),
         )
     elif query.data == "Music_roy_play":
-        query.message.edit_caption(f"*✿ ᴄᴏᴍᴍᴀɴᴅꜱ ✿*",
+        query.message.edit_caption(f"*✿ ᴄᴏᴍᴍᴀɴᴅꜱ ✿*"
             f"""\n\n ᴄᴏᴍᴍɪɴɢ sᴏᴏɴ""",
             parse_mode=ParseMode.MARKDOWN,
             
