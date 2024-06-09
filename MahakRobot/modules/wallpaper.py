521883ed66a70727642ecccf8ffe353e6ba397a5
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-09 15:27:45 +0530

    Update wallpaper.py

--- MahakRobot/modules/wallpaper.py
+++ MahakRobot/modules/wallpaper.py
@@ -21,7 +21,7 @@ async def wall(_, message: Message):
         ran = random.randint(0, 3)
         await message.reply_photo(
             photo=url[ran]["imageUrl"],
-            caption=f"⬤ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐",
+            caption=f"⬤ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴍᴀʜᴀᴋ ꭙ ʀᴏʙᴏᴛ™ ♡゙",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("ʟɪɴᴋ", url=url[ran]["imageUrl"])],
