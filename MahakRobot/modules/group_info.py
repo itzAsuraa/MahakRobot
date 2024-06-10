650b7a3f23f978f6af0cf3ff1643bf43b43b9496
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-10 19:58:21 +0530

    Update group_info.py

--- MahakRobot/modules/group_info.py
+++ MahakRobot/modules/group_info.py
@@ -32,7 +32,7 @@ async def get_group_status(_, message: Message):
         f"● ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs ➥ {total_members}\n"
         f"● ᴜsᴇʀɴᴀᴍᴇ ➥ @{group_username}\n"
         f"● ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ➥ \n{group_description or 'N/A'}\n\n"
-        f"❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐"
+        f"❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴍᴀʜᴀᴋ ꭙ ʀᴏʙᴏᴛ™ ♡゙"
     )
     
     await message.reply((response_text),reply_markup=InlineKeyboardMarkup(EVAA),)
@@ -51,7 +51,7 @@ def group_status(client, message):
                   f"● ᴛʏᴘᴇ ➥ {chat.type}\n"
                   
     if chat.username:  # Not all groups have a username
-        status_text += f"● ᴜsᴇʀɴᴀᴍᴇ ➥ @{chat.username}\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐"
+        status_text += f"● ᴜsᴇʀɴᴀᴍᴇ ➥ @{chat.username}\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴍᴀʜᴀᴋ ꭙ ʀᴏʙᴏᴛ™ ♡゙"
     else:
         status_text += "Username: None"
 
