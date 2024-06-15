9eb6d563df9831ccc1a6b5c9f546eb4ec77f6911
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-15 21:17:15 +0530

    Update group_info.py

--- MahakRobot/modules/group_info.py
+++ MahakRobot/modules/group_info.py
@@ -1,10 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message 
 from MahakRobot import pbot as app
+from MahakRobot import BOT_USERNAME
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Mahakxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
     ],
 ]
 
