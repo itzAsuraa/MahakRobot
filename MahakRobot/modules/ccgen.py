811d231004ad71fbdf598775903f2014b3a1e5cf
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-15 21:56:41 +0530

    Update ccgen.py

--- MahakRobot/modules/ccgen.py
+++ MahakRobot/modules/ccgen.py
@@ -35,7 +35,7 @@ async def gen_cc(client, message):
 `{cards[6]}`\n`{cards[7]}`\n`{cards[8]}`
 `{cards[9]}`
 
-❖ ᴄᴄ ɢᴇɴ ʙʏ ➥ ๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐""" , reply_markup=InlineKeyboardMarkup(EVAA),
+❖ ᴄᴄ ɢᴇɴ ʙʏ ➥ ˹ ᴍᴀʜᴀᴋ ꭙ ʀᴏʙᴏᴛ™""" , reply_markup=InlineKeyboardMarkup(EVAA),
         )
     except Exception as e:
         return await aux.edit(f"❖ ᴇʀʀᴏʀ ➥ {e}")
