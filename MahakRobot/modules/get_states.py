f750a38dd53e2b45cb151e84f7a371dce4dded43
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-15 23:36:43 +0530

    Update get_states.py

--- MahakRobot/modules/get_states.py
+++ MahakRobot/modules/get_states.py
@@ -1,12 +1,13 @@
 from pyrogram import Client, filters
 import pycountry
 from MahakRobot import pbot as app 
+from MahakRobot import BOT_USERNAME
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Mahakxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
     ],
 ]
 
