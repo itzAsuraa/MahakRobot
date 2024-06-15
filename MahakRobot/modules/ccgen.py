bfa7b9967c2157848d370e7ad112bb3f57c0477b
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-15 22:37:51 +0530

    Update ccgen.py

--- MahakRobot/modules/ccgen.py
+++ MahakRobot/modules/ccgen.py
@@ -3,10 +3,11 @@ from pyrogram import *
 from pyrogram.types import *
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from MahakRobot import pbot as app 
+from MahakRobot import BOT_USERNAME
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Mahakxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
     ],
 ]
 
