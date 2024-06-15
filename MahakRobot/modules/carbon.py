6743ba64dda2385528c9b79408fbea8045e37e3d
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-15 23:50:39 +0530

    Update carbon.py

--- MahakRobot/modules/carbon.py
+++ MahakRobot/modules/carbon.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/mahakxbot?startgroup=true"),
     ],
 ]
 
