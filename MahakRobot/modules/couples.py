779d34155a44a27e9f4d0fd21cacc711d2776f57
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-15 11:29:19 +0530

    Update couples.py

--- MahakRobot/modules/couples.py
+++ MahakRobot/modules/couples.py
@@ -14,7 +14,7 @@ AVISHA = [
     [
         InlineKeyboardButton(
             text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ",
-            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            url=f"https://t.me/avishaxbot?startgroup=true",
         ),
     ],
 ]
