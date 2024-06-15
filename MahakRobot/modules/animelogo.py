e770de066ffbdea459e19fb5d6e3db76cc79b955
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-15 21:25:36 +0530

    Update animelogo.py

--- MahakRobot/modules/animelogo.py
+++ MahakRobot/modules/animelogo.py
@@ -5,12 +5,13 @@ import pyrogram, asyncio, random, time
 from pyrogram.errors import FloodWait
 import requests
 from MahakRobot import pbot as app
+from MahakRobot import BOT_USERNAME
 from pyrogram.types import *
 
 button = [
        [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/MahakXbot?startgroup=true",
+                text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ]
 ]
