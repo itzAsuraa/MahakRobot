4b6305fab911fd56d2ccdd12cb4af7f90109c3bb
GOD RAVAN <167273277+GOD-R4V4N@users.noreply.github.com>
2024-06-15 22:16:54 +0530

    Update hastag.py

--- MahakRobot/modules/hastag.py
+++ MahakRobot/modules/hastag.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup as  BSP
 from MahakRobot import pbot as app
+from MahakRobot import BOT_USERNAME
 from pyrogram.types import Message,InlineKeyboardButton,InlineKeyboardMarkup
 from pyrogram import filters
 
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Mahakxbot?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
     ],
 ]
 
