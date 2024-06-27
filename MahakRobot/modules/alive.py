849b1b5cd8ab2a5f69b7dd6b1ffddc9508130863
𖤍ᴛʜᴇ / ᴄᴀᴘᴛᴀɪɴ𖤍 <167273277+itzAsuraa@users.noreply.github.com>
2024-06-27 13:26:20 +0530

    Update alive.py

--- MahakRobot/modules/alive.py
+++ MahakRobot/modules/alive.py
@@ -35,7 +35,7 @@ Avisha = [
     [
         InlineKeyboardButton(
             text="ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
-        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/god_ravana")
+        InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/Ravan_Lankaa")
     ],
 ]
 
